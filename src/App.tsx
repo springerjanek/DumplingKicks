08f496ea33f378628e2f0f19382f68f9c4c6e9a8
Perke1 <springerjan2@gmail.com>
2023-04-27 14:18:05 +0200

    changed to coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Main from "./components/Main";
-import ProductPage from "./components/ProductPage";
-import Navbar from "./components/Navbar";
-import Contact from "./components/Contact";
+// import ProductPage from "./components/ProductPage";
+// import Navbar from "./components/Navbar";
+// import Contact from "./components/Contact";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
-          <Route path="*" element={<Navigate replace to={"/"} />} />
+          {/* <Route path="*" element={<Navigate replace to={"/"} />} /> */}
           <Route path="/" element={<Main />} />
-          <Route path="/product/:name" element={<ProductPage />} />
-          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/product/:name" element={<ProductPage />} />
+          <Route path="/contact" element={<Contact />} /> */}
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,27 +1,46 @@
 import React from "react";
-import data from "../shoes.json";
-import { useNavigate } from "react-router-dom";
+// import data from "../shoes.json";
+// import { useNavigate } from "react-router-dom";
+// import { Button, Grid } from "@mui/material";
+
 const Main = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
-    <div className="landing-container">
-      {data.map((shoe) => {
-        const { id, name, sizes_and_prices, sku, thumbnail } = shoe;
-        console.log(name.replace(/\s/g, "-"));
-        return (
-          <div className="shoe-container" key={id}>
-            <img
-              src={thumbnail}
-              alt="Product"
-              onClick={() =>
-                navigate(`/product/${name.replace(/\s/g, "-").toLowerCase()}`)
-              }
-            ></img>
-          </div>
-        );
-      })}
+    <div className="contact">
+      <h1>COMING SOON</h1>
+      <div className="ig">
+        <h2>CHECK OUT OUR IG</h2>
+        <a href="https://www.instagram.com/dumpling_kicks">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png"
+            style={{
+              width: "35px",
+              height: "35px",
+              marginTop: "27px",
+              marginLeft: "10px",
+            }}
+          />
+        </a>
+      </div>
     </div>
+    // <Grid container columnGap={6}>
+    //   {data.map((shoe) => {
+    //     const { id, name, sizes_and_prices, sku, thumbnail } = shoe;
+    //     console.log(name.replace(/\s/g, "-"));
+    //     return (
+    //       <Grid>
+    //         <img
+    //           src={thumbnail}
+    //           alt="Product"
+    //           onClick={() =>
+    //             navigate(`/product/${name.replace(/\s/g, "-").toLowerCase()}`)
+    //           }
+    //         ></img>
+    //       </Grid>
+    //     );
+    //   })}
+    // </Grid>
   );
 };
 
